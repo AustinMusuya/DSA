9791c771571db2aeb446eac39781aa17828546fd
austinmusuya <amusuya57@gmail.com>
2025-02-22 13:09:39 +0300

    create optimized approach in python

--- First Recurring Character/exercise.py
+++ First Recurring Character/exercise.py
@@ -23,6 +23,27 @@ def firstRecurring(input):
 
 
 
-print(firstRecurring([2,5,1,2,3,5,1,2,4]))
-print(firstRecurring([2,1,1,2,3,5,1,2,4]))
-print(firstRecurring([2,3,4,5]))
+# print(firstRecurring([2,5,1,2,3,5,1,2,4]))
+# print(firstRecurring([2,1,1,2,3,5,1,2,4]))
+# print(firstRecurring([2,3,4,5]))
+
+# Optimized approach (hashmap)
+
+# Approach: 
+# Loop through all the array elements and store in hashmap. 
+# Upon looping ,Check if the key already exists in the map and return it. 
+# Else add the element as the key, and its index as the value
+
+def firstRecurring2(input):
+    hashmap = {}
+    for i in range(len(input)):
+        if input[i] in hashmap:
+            return input[i]
+        else:
+            hashmap[input[i]] = i
+        
+    return None
+
+print(firstRecurring2([2,5,1,2,3,5,1,2,4]))
+print(firstRecurring2([2,1,1,2,3,5,1,2,4]))
+print(firstRecurring2([2,3,4,5]))
