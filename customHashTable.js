7f3a644f03497f35c9748a3dc2ff9bc8a672932f
austinmusuya <amusuya57@gmail.com>
2025-02-22 08:13:15 +0300

    add method for setting a key in our custom hashtable

--- customHashTable.js
+++ customHashTable.js
@@ -10,10 +10,39 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    let address = this._hash(key);
+    let item = [key, value];
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push(item);
+
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    let currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] == key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-myHashTable.get("grapes");
+// myHashTable.get("grapes");
 myHashTable.set("apples", 9);
 myHashTable.get("apples");
+
+console.log(myHashTable);
+// expected output
+
+// HashTable { data: [ <50 empty items> ["grapes", 10000],["apples", 5000],["mangoes", 55] ] }
