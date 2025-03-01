1fdef7d6079db4a3158ac4f5a26d7b8270b9ad26
austinmusuya <amusuya57@gmail.com>
2025-03-01 16:50:24 +0300

    create dunder str method and pop method logic for linked list stack

--- stacks + queues/LinkedListStack.py
+++ stacks + queues/LinkedListStack.py
@@ -14,23 +14,37 @@ class Stack():
         return self.top
 
     def push(self, value):
-        newNode = {
-            'value': value,
-            'next': None
-        }
+        newNode = Node(value)
         if self.length == 0:
             self.top = newNode
             self.bottom = newNode
         else:
             holdingPoint = self.top
             self.top = newNode
-            self.top['next'] = holdingPoint
+            self.top.next = holdingPoint
 
         self.length += 1
         return self
 
     def pop(self):
-        pass
+       if self.top is None:
+           return None
+       if self.top == self.bottom:
+           self.bottom = None
+
+       popped_value = self.top.value
+       self.top = self.top.next
+       self.length -= 1
+       return popped_value
+    
+    def __str__(self):
+        array = []
+        currentNode = self.top
+        while currentNode is not None:
+            array.append(currentNode.value)
+            currentNode = currentNode.next
+
+        return f"Stack {array} | Top: {self.top.value} | Bottom: {self.bottom.value}"
 
 
 myStack = Stack()
@@ -39,5 +53,7 @@ myStack.push(50)
 myStack.push(40)
 myStack.push(80)
 myStack.push(16)
+# myStack.pop()
+
 
 print(myStack)
