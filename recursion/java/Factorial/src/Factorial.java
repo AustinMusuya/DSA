f5d810a83421b2dc0534a0edd87ca6cb49e0e3ae
AustinMusuya <amusuya57@gmail.com>
2025-03-12 10:17:52 +0300

    createa  a rpogram to find the factorial of a given number using iterative and recursive approach.

---
+++ recursion/java/Factorial/src/Factorial.java
@@ -0,0 +1,68 @@
+public class Factorial {
+
+    // Steps to achieve recursion
+
+// 1. Identify the base case of the function
+// so we can opt out when the condition is met and avoid filling up the callstack
+// 2. Identify the recursive case (where we call the function within our function)
+// 3. Get closer and closer and return when needed (so this means we'll have 2 return statements):
+//      each for the base case and recursive case
+
+/*
+Example of Recursion use case
+
+public static void inception = () => {
+    int counter = 0;
+    if (counter > 3) { // The if statement is the base case and returns done when the condition is met
+        System.out.println("Done!");
+    }
+    inception() //This is the recursive case (function called within the function)
+}
+
+*/
+
+
+    public static void main(String[] args) {
+        // Exercise:
+        // Use recursion to get factorial of a number passed as the argument on your function call
+        // Have two approaches the recursive approach & iterative approach
+
+
+        // iterative Approach:
+
+        System.out.println(findFactorial(35));
+
+        // Recursive Approach:
+
+//        System.out.println(findFactorial2(8));
+
+    }
+
+    // Iterative approach
+    public static int findFactorial(int number){
+        if (number < 0){
+            return 0;
+        }
+        int temp = number - 1;
+        int result = number;
+
+        while (temp > 0){
+            result *= temp;
+            temp--;
+        }
+        return result;
+    }
+
+    // Recursive approach
+    public static int findFactorial2(int number){
+        if (number < 0){
+            return 0;
+        }
+        if (number == 1 || number == 0){
+            return 1;
+        }
+        return findFactorial2(number - 1) * number;
+    }
+
+
+}
