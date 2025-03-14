9c4373162db285bb9f06fb54f41ed423cedc5d3f
AustinMusuya <amusuya57@gmail.com>
2025-03-14 18:17:38 +0300

    optimize bubble sort algorithm

--- algorithms/sorting algorithms/javascript/bubbleSort.js
+++ algorithms/sorting algorithms/javascript/bubbleSort.js
@@ -1,6 +1,6 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(array) {
+function bubbleSort(array) { //Not really bubble sort but a good effort in most cases
   //Code here
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j < array.length; j++) {
@@ -13,6 +13,28 @@ function bubbleSort(array) {
   }
   return array;
 }
+
+const theRealBubbleSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    swapped = false; // Set a boolean flag to track swaps
+    for (let j = 0; j < array.length - 1 - i; j++) {//Reduce the range after every outer loop iteration
+      if (array[j] > array[j + 1]) {
+        // Swap adjacent Elements
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    } //Exit if no swaps (already sorted)
+  }
+  return array;
+};
+
 console.log(`unsorted array : ${numbers}`);
-bubbleSort(numbers);
+
+theRealBubbleSort(numbers);
+
 console.log(`sorted array : ${numbers}`);
