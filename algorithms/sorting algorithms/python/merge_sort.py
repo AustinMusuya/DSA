cff6b77fc36cb1c296b46dd76154ba73f193d5b6
AustinMusuya <amusuya57@gmail.com>
2025-03-24 12:57:33 +0300

    add descriptive breakdown of the mergesort algorithm

--- algorithms/sorting algorithms/python/merge_sort.py
+++ algorithms/sorting algorithms/python/merge_sort.py
@@ -1,35 +1,115 @@
+
+
+# Merge sort
+'''
+The approach here is divide and conquer. We first split the array into two halves recursively
+until we are left with individual elements in single arrays. We then compare values on which is less
+where, the lesser value is pushed to a new array first and the latter is pushed thereafter.
+The array is then merged or unwound iteratively with such comparisons till
+we get the full array as sorted
+
+
+Visualized breakdown and merge for mergesort algorithm
+
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+# left              #right
+[99, 44, 6, 2, 1,],  [5, 63, 87, 283, 4, 0]
+
+# Left side
+
+# splitting
+[99, 44, 6, 2, 1]
+
+[99, 44], [6, 2, 1 ],
+
+[99], [44], [6], [2, 1 ]
+
+[99], [44], [6], [2], [1],
+
+# merging
+
+[44, 99] , [6], [2], [1],
+
+[44, 99] , [6], [1, 2]
+
+[44, 99] , [1,2,6]
+
+[1, 2, 6, 44, 99]
+
+# Right side
+
+# splitting
+[5, 63, 87, 283, 4, 0]
+
+[5, 63, 87] [283, 4, 0]
+
+[5] [63, 87] [283], [4, 0]
+
+[5], [63], [87] [283], [4], [0]
+
+# merging
+[5], [63,87], [283] [0, 4]
+
+[5, 63, 87], [0, 4, 283]
+
+[0, 4, 5, 63, 87, 283]
+
+# Left                           #Right
+[1, 2, 6, 44, 99]               [0, 4, 5, 63, 87, 283]
+
+[0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283]
+
+'''
+
+
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 
 def merge_sort(array):
-    if len(array) <= 1:  # base case
+    #   base case
+    if len(array) <= 1:
         return array
 
-    # get the middle of the array
+    # divide and conquer
     middle = len(array) // 2
     left = array[:middle]
     right = array[middle:]
+
     return merge(merge_sort(left), merge_sort(right))  # recursive case
 
 
 def merge(left, right):
-    left_index = 0
-    right_index = 0
+    leftIndex = 0
+    rightIndex = 0
     result = []
 
-    while left_index < len(left) and right_index < len(right):
-
-        if (left[left_index] < right[right_index]):
-            result.append(left[left_index])
-            left_index += 1
+    while leftIndex < len(left) and rightIndex < len(right):
+        if left[leftIndex] < right[rightIndex]:
+            result.append(left[leftIndex])
+            leftIndex += 1
         else:
-            result.append(right[right_index])
-            right_index += 1
+            result.append(right[rightIndex])
+            rightIndex += 1
 
-    result.extend(left[left_index:])
-    result.extend(right[right_index:])
+    # adding the remaining elements
+
+    while leftIndex < len(left):
+        result.append(left[leftIndex])
+        leftIndex += 1
+
+    while rightIndex < len(right):
+        result.append(right[rightIndex])
+        rightIndex += 1
+
+    # result.extend(left[leftIndex:]) #cleaner
+    # result.extend(right[rightIndex:])
 
     return result
 
 
 print(merge_sort(numbers))
+
+# print(numbers[0: len(numbers)//2])
+
+# print(numbers[len(numbers)//2: len(numbers)])
