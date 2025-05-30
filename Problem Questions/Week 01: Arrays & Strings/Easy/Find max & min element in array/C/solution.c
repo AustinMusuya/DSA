a8dd6cdc02618953579f6f452fbdf4acbb0ebae4
AustinMusuya <amusuya57@gmail.com>
2025-05-30 10:55:48 +0300

    solve: implement find max and min logic in c.

--- Problem Questions/Week 01: Arrays & Strings/Easy/Find max & min element in array/C/solution.c
+++ Problem Questions/Week 01: Arrays & Strings/Easy/Find max & min element in array/C/solution.c
@@ -3,18 +3,18 @@
 
 ## Problem Statement
 
-Given an array of integers, 
-return the **maximum** and **minimum** 
+Given an array of integers,
+return the **maximum** and **minimum**
 elements in the array.
 
 ---
 
 ## Example
 
-**Input:**  
+**Input:**
 `nums = [3, 1, 5, 9, 0, -3]`
 
-**Output:**  
+**Output:**
 `max = 9, min = -3`
 
 ---
@@ -22,10 +22,41 @@ elements in the array.
 
 #include <stdio.h>
 
-int main(){
-    // Approach: grab a page off selection sort logic
-    // Step1: initialize a max and min variable at index 0
-    // Step2: loop through array updating the values,
-    // when a new max or min is found.
+// function prototype:
+void findMaxMin(int *array, int length, int *min, int *max);
+
+int main()
+{
+    // usage
+    int nums[] = {21, 78, 32, 4, 6, 23, 3, 12, 1, 2};
+    int min, max;
+    int length = sizeof(nums) / sizeof(nums[0]);
+
+    findMaxMin(nums, length, &min, &max);
+
+    printf("Min: %d, Max: %d\n", min, max);
+
     return 0;
+}
+
+// Approach: Grab a page off selection sort logic
+void findMaxMin(int *array, int length, int *min, int *max)
+{
+    // Step 1: Initialize a max and min variable at index 0
+    *min = array[0];
+    *max = array[0];
+
+    // Step 2: Loop through array updating the values,
+    // when a new max or min is found.
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] < *min)
+        {
+            *min = array[i];
+        }
+        if (array[i] > *max)
+        {
+            *max = array[i];
+        }
+    }
 }
