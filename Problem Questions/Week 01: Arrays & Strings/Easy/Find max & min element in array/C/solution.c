fb50f54a128e35709240eb339157f730b7510d26
austinmusuya <amusuya57@gmail.com>
2025-05-30 08:02:55 +0300

    solve: initialize starting boiler plate code to solve find max and min element problem in C

---
+++ Problem Questions/Week 01: Arrays & Strings/Easy/Find max & min element in array/C/solution.c
@@ -0,0 +1,31 @@
+/*
+# Find Maximum and Minimum in Array
+
+## Problem Statement
+
+Given an array of integers, 
+return the **maximum** and **minimum** 
+elements in the array.
+
+---
+
+## Example
+
+**Input:**  
+`nums = [3, 1, 5, 9, 0, -3]`
+
+**Output:**  
+`max = 9, min = -3`
+
+---
+*/
+
+#include <stdio.h>
+
+int main(){
+    // Approach: grab a page off selection sort logic
+    // Step1: initialize a max and min variable at index 0
+    // Step2: loop through array updating the values,
+    // when a new max or min is found.
+    return 0;
+}
